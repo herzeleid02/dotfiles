ea7fd5abecf551ea920b76c9756fea0e5bc12572
herzeleid02 <81138645+herzeleid02@users.noreply.github.com>
2024-03-26 21:49:35 +0300

    fixed my install script and added vim-plug installation for neovim

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-ln -isv $(realpath ./mpv/) ~/.config/mpv
-ln -isv $(realpath ./nvim/) ~/.config/nvim
+# i didnt test it on anything posix. it should work with gnu coreutils
+
+ln -isvn $(realpath ./mpv/) ~/.config/mpv
+ln -isvn $(realpath ./nvim/) ~/.config/nvim
+
+if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ] && [ command -v nvim ] ; then
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
